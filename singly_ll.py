1c724e6de1b18e9628bbb645abfa84504912deaa
Abhishek <abhibhovar@gmail.com>
2024-05-12 02:14:45 +0530

    Added a class to handle all the functions related to Linkedlist and defined functions to be implemented

--- singly_ll.py
+++ singly_ll.py
@@ -19,3 +19,49 @@ class Node:
         """
         self.data = data
         self.next_node = next_node
+
+
+# Creating a class to handle all the linked list operations
+class LinkedList:
+    """
+        Following class will handle all the functions related to processing of linked list
+    """
+    def __init__(self, data):
+        """
+        Will handle generation of a initial Node, which will also be the head and tial of the LinkedList
+
+        Args:
+            data (_type_): _description_
+        """
+        self.head:Node = Node(data=data)
+        self.tail:Node = self.head
+
+
+    # Method to append a new node
+    def add_node(self, data:any=None):
+        pass    
+
+
+    # Method to insert a new node
+    def insert_node(self, position:int=-1, data:any=None):
+        pass
+
+
+    # Method to remove node at a position
+    def remove_node(self, position:int=-1):
+        pass
+
+
+    # Method to reverse the list
+    def reverse_list(self):
+        pass
+
+    
+    # Method to display the created list in CMD
+    def display(self, alignment:str='h'):
+        pass
+
+
+    # Method to find the nth element
+    def find_n_element(self):
+        pass
